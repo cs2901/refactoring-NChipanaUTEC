17ecd20732d35c9428fddc256b789f7ef6d2af64
Nicolás <nicolas.chipana@utec.edu.pe>
2019-06-03 09:43:30 -0500

    Refactor method and variable

--- src/composing/extract-variable_before.java
+++ src/composing/extract-variable_before.java
@@ -1,7 +1,7 @@
 void renderBanner() {
     final boolean index_MAC = (platform.toUpperCase().indexOf("MAC");
     final boolean index_IE =  (browser.toUpperCase().indexOf("IE");
-    if (index_MAC > -1) && (index_IE > -1) &&
+    if ((index_MAC > -1) && (index_IE > -1) &&
         wasInitialized() && resize > 0 )
   {
     // do something
